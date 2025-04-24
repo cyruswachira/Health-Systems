1728362c7361f88fb6d357ab54db11101822d5ec
CYRUS WACHIRA <mugowachira254@gmail.com>
2025-04-24 13:41:04 +0300

    dashboard pages created

---
+++ health-system-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <p>Welcome to the Health Information System</p>
+      </div>
+    );
+  };
